cb168b0e7d1590829dc01a61fbdb8f88a18bc49e
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-03-26 15:56:08 +0100

    change card size to fit content

--- frontend/src/routes/project/[slug]/MetricCard.svelte
+++ frontend/src/routes/project/[slug]/MetricCard.svelte
@@ -2,7 +2,7 @@
 	import { Card, Heading } from 'flowbite-svelte';
 </script>
 
-<Card class="max-w-xl">
+<Card class="max-w-fit">
 	<Heading tag="h5" class="mb-4"><slot name="heading" /></Heading>
 	<!-- https://stackoverflow.com/questions/72432862/how-to-set-min-content-for-a-column-in-tailwind-css-grid -->
 	<div class="grid gap-y-4 gap-x-12 max-w-xl grid-cols-[max-content_max-content]">
