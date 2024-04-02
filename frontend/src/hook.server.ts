932d9145f5d293ef9efd0f28bacf1256a522c91a
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-04-02 19:44:37 +0200

    added certificates to frontend

--- frontend/src/hook.server.ts
+++ frontend/src/hook.server.ts
@@ -1,11 +1,13 @@
 import { Database, aql } from "arangojs";
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
+console.log("development: "+ dev);
 
 export const db = new Database({
     url: env.ARANGO_URL,
     databaseName: "crossd",
     auth: { username: env.FRONTEND_USER ?? "", password: env.FRONTEND_PASSWORD },
     agentOptions: {
-        rejectUnauthorized: false
+        rejectUnauthorized: dev ? false : true
     },
 });
