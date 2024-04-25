468f5b4b4608f6fb1d01de85dedc84f181c92e5e
tdam <20456596+tobiasdam@users.noreply.github.com>
2024-04-25 17:51:01 +0200

    fix legal information link

--- frontend/src/routes/legal-information/+page.svelte
+++ frontend/src/routes/legal-information/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
-    import { Heading, P, A, Mark, Secondary } from 'flowbite-svelte';
+	import { Heading, P, A, Mark, Secondary } from 'flowbite-svelte';
 </script>
 
 <Heading tag="h1">Legal Information</Heading>
-<P class="mt-10">Link to the legal notice of St. Pölten University of Applied Sciences: <A>https://www.fhstp.ac.at/en/legal-information</A></P>
+<P class="mt-10"
+	>Link to the legal notice of St. Pölten University of Applied Sciences: <A
+		href="https://www.fhstp.ac.at/en/legal-information"
+		>https://www.fhstp.ac.at/en/legal-information</A
+	></P
+>
