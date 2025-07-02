4abafaa408630c50b685ce7931dd6db9109c1d22
tdam <20456596+tobiasdam@users.noreply.github.com>
2025-07-02 15:54:39 +0200

    added query for errors

--- tools/db.py
+++ tools/db.py
@@ -4,6 +4,7 @@
 import argparse
 import datetime
 import getpass
+from pprint import pprint
 from urllib.parse import urlparse
 
 import pyArango.connection
@@ -27,14 +28,17 @@ def duration(conn, args):
     FILTER !(false in y)
     FOR doc IN task_results
     FILTER CONTAINS(doc.task, CONCAT(scan._id))
-    LET task = JSON_PARSE(doc.task)
+    LET task = JSON_PARSE(doc.task){task_name}
     SORT task.date_done
     FILTER task.date_done
     RETURN task.date_done)
     RETURN [first(x), last(x)]
-    """
-
+    """.format(
+        task_name=f"\nFILTER task.name == @name" if args.task_name else ""
+    )
     vars = {"tags": args.tags}
+    if args.task_name:
+        vars["name"] = args.task_name[0]
     res = conn["crossd"].AQLQuery(query, rawResults=True, bindVars=vars)
     if res[0] == [None, None]:
         print("No results found")
@@ -71,6 +75,20 @@ def get_not_found(conn, args):
         print(elem)
 
 
+def errors_for_found(conn, args):
+    query = """
+    FOR res IN task_results
+    FILTER res.task LIKE '%FAILURE%'
+    FILTER res.task NOT LIKE '%NOT_FOUND%'
+    LET x = JSON_PARSE(res.task)
+    RETURN {"date":x.date_done,"args":x.args, "type": x.result.exc_type, "msg": x.result.exc_message}
+    """
+    res = conn["crossd"].AQLQuery(query, rawResults=True)
+    for elem in res:
+        pprint(elem)
+        print()
+
+
 def main(args):
     if any([args.password in [None, "-"]]):
         args.password = getpass.getpass("Arango Password: ")
@@ -103,7 +121,10 @@ if __name__ == "__main__":
     parser_duration = subparsers.add_parser(
         "duration", description="Get the duration of a scan"
     )
-    parser_duration.add_argument(help="task to search for", dest="tags", nargs="+")
+    parser_duration.add_argument(help="tag to search for", dest="tags", nargs="+")
+    parser_duration.add_argument(
+        "-n", "--task-name", help="search for specific task", nargs=1
+    )
     parser_duration.set_defaults(func=duration)
 
     parser_tags = subparsers.add_parser(
@@ -115,6 +136,10 @@ if __name__ == "__main__":
         "notfound", description="Get all repositories that were not found"
     )
     parser_tags.set_defaults(func=get_not_found)
+    parser_tags = subparsers.add_parser(
+        "errorfound", description="Get errors for repositories that exist"
+    )
+    parser_tags.set_defaults(func=errors_for_found)
 
     args = parser.parse_args()
     main(args)
