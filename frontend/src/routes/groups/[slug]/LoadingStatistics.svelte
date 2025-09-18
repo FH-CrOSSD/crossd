77ca37e6081026a8acb917ef412cdabab826ceb5
tdam <20456596+tobiasdam@users.noreply.github.com>
2025-09-18 13:13:40 +0200

    fix white background on loading for tabs

--- frontend/src/routes/groups/[slug]/LoadingStatistics.svelte
+++ frontend/src/routes/groups/[slug]/LoadingStatistics.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <Tabs style="underline" class="w-full" contentClass="bg-white dark:bg-gray-800">
-	<TabItem open class="bg-white">
+	<TabItem open class="">
 		{#snippet titleSlot()}
 			<div class="mb-2.5 h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700 animate-pulse"></div>
 		{/snippet}
@@ -15,7 +15,7 @@
 			<WidgetPlaceholder class="w-1/3" />
 		</div>
 	</TabItem>
-	<TabItem class="bg-white" disabled>
+	<TabItem class="" disabled>
 		{#snippet titleSlot()}
 			<div class="mb-2.5 h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700 animate-pulse"></div>
 		{/snippet}
