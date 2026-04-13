edd9c33ee0b8c007a547309faa0a9d0c6e506c65
tdam <20456596+tobiasdam@users.noreply.github.com>
2026-04-13 20:40:28 +0200

    added settings menu

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import {
+	Button,
 		DarkMode,
+		Dropdown,
 		Footer,
 		FooterCopyright,
 		FooterIcon,
@@ -9,12 +11,13 @@
 		NavBrand,
 		NavLi,
 		NavUl,
-		Navbar
+		Navbar,
+		Toggle
 	} from 'flowbite-svelte';
 	// import '../app.pcss';
 	import '../app.css';
 	import { onMount } from 'svelte';
-	import { ArrowUpRightFromSquareOutline, GithubSolid } from 'flowbite-svelte-icons';
+	import { ArrowUpRightFromSquareOutline, CogSolid, GithubSolid } from 'flowbite-svelte-icons';
 	// import { theme } from '$lib/util';
 
 	const btnClass =
@@ -36,9 +39,31 @@
 				// theme.set('light');
 			// }
 		}
+		futureFont = localStorage.getItem('futuristic-font');
 	});
+
+		function toggleMode() {
+		document.documentElement.classList.toggle('dark');
+		localStorage.setItem(
+			'THEME_PREFERENCE_KEY',
+			localStorage.getItem('THEME_PREFERENCE_KEY') != 'dark' ? 'dark' : 'light'
+		);
+		// theme.set(localStorage.getItem('THEME_PREFERENCE_KEY'));
+	}
+
+	function toggleFont() {
+		if (localStorage.getItem('futuristic-font')) {
+			localStorage.removeItem('futuristic-font');
+			futureFont = null;
+		} else {
+			localStorage.setItem('futuristic-font', 'ubuntu');
+			futureFont = 'ubuntu';
+		}
+	}
+	let futureFont = null;
 </script>
 
+<main class={!futureFont ? 'future-font' : 'normal-font'}>
 <Navbar
 	class="bg-gray-50 dark:bg-gray-900 text-gray-700 dark:text-gray-200 border-gray-100 dark:border-gray-700 px-2 sm:px-4 py-2.5 w-full"
 >
@@ -55,8 +80,22 @@
 		<NavLi href="https://fh-crossd.github.io/">Docs<ArrowUpRightFromSquareOutline class="inline h-4 mb-0.5" /></NavLi>
 		<NavLi href="https://crossd.tech">Project & Blog<ArrowUpRightFromSquareOutline class="inline h-4 mb-0.5" /></NavLi>
 	</NavUl>
-	<!-- <DarkMode {btnClass} onclick={toggleMode} /> -->
-	<DarkMode {btnClass}/>
+	<!-- <DarkMode {btnClass}/> -->
+	<Button color="none" class="{btnClass}"><CogSolid /></Button>
+				<Dropdown class="w-56 space-y-1 p-3">
+						<Toggle
+							color="gray"
+							class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-500"
+							checked={!Boolean(localStorage.getItem('futuristic-font'))}
+							onclick={toggleFont}>Futuristic Font</Toggle
+						>
+						<Toggle
+							onclick={toggleMode}
+							color="gray"
+							checked={localStorage.getItem('THEME_PREFERENCE_KEY') === 'dark'}
+							class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-500">Dark Mode</Toggle
+						>
+				</Dropdown>
 </Navbar>
 <div class="overflow-y-auto p-0 m-0 h-[calc(76vh))] min-h-fit">
 <div class="container mx-auto my-10 ">
@@ -92,6 +131,7 @@
 		</FooterIcon>
 	</FooterLinkGroup>
 </Footer>
+</main>
 
 <style>
 		/* :global(:root) {
@@ -114,10 +154,29 @@
 		font-family: Anta;
 		src: url('/Anta-Regular.ttf');
 	}
-	.heading {
+	@font-face {
+		font-family: ubuntu;
+		src: url('/ubuntu.regular.ttf');
+	}
+	@font-face {
+		font-family: ubuntu-mono;
+		src: url('/ubuntu.mono.ttf');
+	}
+	@font-face {
+		font-family: ubuntu-mono-bold;
+		src: url('/ubuntu.mono-bold.ttf');
+	}
+	/* .heading {
 		font-family: 'Anta';
 	}
 	:global(div) {
 		font-family: 'Anta';
+	} */
+
+	:global(.future-font) {
+		font-family: 'Anta';
+	}
+	:global(.normal-font) {
+		font-family: 'ubuntu';
 	}
 </style>
